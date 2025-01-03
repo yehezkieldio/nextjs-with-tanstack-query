f5a49b91e4d29306411ef5a0325d1fd86e9bf9bb
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-01-03 14:45:43 +0800

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div>
+            <div className="p-4">
+                <h1 className="text-xl">Hello, world!</h1>
+            </div>
+        </div>
+    );
+}
